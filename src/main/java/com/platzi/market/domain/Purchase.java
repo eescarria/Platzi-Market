521a94e95bea74bfc2d12295b620a3a89df7b63c
eescarria} <estefy0114@gmail.com>
2023-04-20 17:59:18 -0500

    Se inicia con Purchase

---
+++ src/main/java/com/platzi/market/domain/Purchase.java
@@ -0,0 +1,9 @@
+package com.platzi.market.domain;
+
+import java.time.LocalDateTime;
+
+public class Purchase {
+    private int purchaseId;
+    private String clientId;
+    private LocalDateTime date;
+}
